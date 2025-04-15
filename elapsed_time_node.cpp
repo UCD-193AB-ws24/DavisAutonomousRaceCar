2eb28108e9736cf395d279001a07a0ea9d156393
GrantNam <102392441+GrantNam@users.noreply.github.com>
2025-04-15 13:20:22 -0700

    Unnecessary test files were removed

--- elapsed_time_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "elapsed_time_publisher.cpp"
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ElapsedTimePublisher>());
-    rclcpp::shutdown();
-    return 0;
-}
--- test_run_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "test_run.cpp"
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<TestRun>());
-    rclcpp::shutdown();
-    return 0;
-}
